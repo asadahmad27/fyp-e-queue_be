2dfa6cd554fc0a4f21275b14e99e577d6e42b868
asadahmad27 <amasadmughal@gmail.com>
2023-01-26 17:46:35 +0500

    headers

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const isAuth = (req, res, next) => {
   const authHeader = req.get('authorization');
+  console.log(res)
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
